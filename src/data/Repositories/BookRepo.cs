e28a706e069722e224806332b5fad980e11e0fbd
Meile5 <meile.kapisaite5@gmail.com>
2026-03-02 11:04:42 +0100

    added book service and book repo

---
+++ src/data/Repositories/BookRepo.cs
@@ -0,0 +1,50 @@
+﻿namespace api.Controllers;
+
+public class BookRepo (AppDbContext dbContext)
+{
+    
+    public async Task<List<Comment>> GetComments(string articleId)
+    {
+        try
+        {
+            return await dbContext.Comments
+                .Include(c => c.CommentUsers)
+                .Where(c => c.ArticleId == articleId)
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+         
+    }
+
+
+    public async Task <List<BookAuthorDto>> GetBooks()
+    {
+        try
+        {
+			return result = await _db.Books
+                .Join(_db.BookAuthors, b => b.Id, ba => ba.BookId, (b, ba) => new { b, ba })
+                .Join(_db.Authors, x => x.ba.AuthorId, a => a.Id, (x, a) => new BookAuthorDto
+                {
+                    BookId = x.b.BookId,
+                    Title = x.b.Title,
+                    AuthorName = a.Name
+                })
+                .ToListAsync();
+            
+			
+            
+        }
+		catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+    }
+    
+    
+}
---
+++ src/data/Services/BookService.cs
@@ -0,0 +1,28 @@
+﻿namespace api.Controllers;
+
+public class BookService (BookRepo repo)
+{
+    
+    public async Task SaveComment(CreateCommentDto createCommentDto)
+    {
+        var comment = new Comment
+        {
+            CommentId = Guid.NewGuid().ToString(),
+            Text = createCommentDto.Comment,
+            ArticleId =  createCommentDto.ArticleId
+        };
+        var hasProfanity = await profanityClient.FilterComment(comment.Text);
+        if (hasProfanity)
+        {
+            throw new Exception("Comment contains forbidden words");
+        }
+        await commentsRepo.SaveComment(comment,  createCommentDto.UserId);
+    }
+
+    public async Task RetrieveBook()
+    {
+        
+    }
+    
+    
+}
